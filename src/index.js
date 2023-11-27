a4c245131a9db789a1071d42e4413f08c59c658e
Shivam Gupta <shivamgupta02005@gmail.com>
2023-11-27 20:18:36 +0530

    Added bannerimage on home page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+import homeImage from './home_banner_image.png';
+
+const main = document.querySelector('.main');
+console.log(main);
+const homeBannerImage = new Image();
+homeBannerImage.setAttribute('class', 'home-banner-image');
+homeBannerImage.src = homeImage;
+main.appendChild(homeBannerImage);
