1b48378d773bdc9c87c40500dd75a27dbfb26c35
Shivam Gupta <shivamgupta02005@gmail.com>
2023-11-28 19:46:28 +0530

    Impoved indentation

--- src/menu.js
+++ src/menu.js
@@ -2,10 +2,10 @@ export default function createMenuPage(){
     const content = document.getElementById('content');
     const pageContent = document.createElement('div');
     const heading = document.createElement('h1');
-    
+
     pageContent.setAttribute('class', 'page-content');
     heading.textContent = 'Menu Page';
     
-    pageContent.appendChild(heading);
+    pageContent.append(heading);
     content.appendChild(pageContent);
 }
