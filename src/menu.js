6002c10678a628cd1c819563224e8e58668dbe68
Shivam Gupta <shivamgupta02005@gmail.com>
2023-11-29 02:21:52 +0530

    Create basic menu page

--- src/menu.js
+++ src/menu.js
@@ -1,11 +1,84 @@
+import pizza from './images/menu/pepperoni pizza.jpg';
+
 export default function createMenuPage(){
     const content = document.getElementById('content');
     const pageContent = document.createElement('div');
-    const heading = document.createElement('h1');
+    const menu = document.createElement('div')
 
+    menu.setAttribute('class', 'menu');
     pageContent.setAttribute('class', 'page-content');
-    heading.textContent = 'Menu Page';
     
-    pageContent.append(heading);
+    menu.appendChild(
+        createMenuItem(
+            'Margerita Pizza',
+            '130',
+            'Lorem Ipusf sdfjlkf df dsf sfsljlk'
+        )
+    );
+    menu.appendChild(
+        createMenuItem(
+            'Margerita Pizza',
+            '130',
+            'Lorem Ipusf sdfjlkf df dsf sfsljlk'
+        )
+    );
+    menu.appendChild(
+        createMenuItem(
+            'Margerita Pizza',
+            '130',
+            'Lorem Ipusf sdfjlkf df dsf sfsljlk'
+        )
+    );
+    menu.appendChild(
+        createMenuItem(
+            'Margerita Pizza',
+            '130',
+            'Lorem Ipusf sdfjlkf df dsf sfsljlk'
+        )
+    );
+    menu.appendChild(
+        createMenuItem(
+            'Margerita Pizza',
+            '130',
+            'Lorem Ipusf sdfjlkf df dsf sfsljlk'
+        )
+    );
+    menu.appendChild(
+        createMenuItem(
+            'Margerita Pizza',
+            '130',
+            'Lorem Ipusf sdfjlkf df dsf sfsljlk'
+        )
+    );
+    
+    pageContent.appendChild(menu);
     content.appendChild(pageContent);
+}
+
+function createMenuItem(name, price, description){
+    const menuItem = document.createElement('div');
+    menuItem.setAttribute('class', 'menu-item');
+
+    const pizzaName = document.createElement('h2');
+    pizzaName.setAttribute('class', 'pizza-name');
+    pizzaName.textContent = name;
+
+    const pizzaDescription = document.createElement('p');
+    pizzaDescription.setAttribute('class', 'description');
+    pizzaDescription.textContent = description;
+
+    const pizzaPrice = document.createElement('p');
+    pizzaPrice.setAttribute('class', 'price');
+    pizzaPrice.textContent = `₹${price}`;
+
+    const pizzaImage = new Image();
+    pizzaImage.setAttribute('class', 'pizza-image');
+    pizzaImage.src = pizza;
+    pizzaImage.alt = 'name';
+
+    menuItem.append(pizzaImage, pizzaName, pizzaPrice, pizzaDescription);
+
+    console.log(menuItem);
+    return menuItem;
+
 }
